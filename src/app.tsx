0423506606586fd5a17fc781117c4d0e4d26d028
Felipe Alencar <me@fecasti.tech>
2023-04-06 09:59:45 -0300

    feat: improve structure and styles

--- src/app.tsx
+++ src/app.tsx
@@ -1,38 +1,12 @@
-import preactLogo from './assets/images/preact.svg';
-import viteLogo from './assets/images/vite.svg';
+import Home from '@pages/Home/Home';
 
 import classes from './app.module.scss';
-import { useState } from 'react';
+import ModalProvider from '@helpers/providers/ModalContext';
 
-export function App() {
-	const [count, setCount] = useState(0);
-
-	return (
-		<>
-			<div>
-				<a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-					<img src={viteLogo} className={classes.logo} alt="Vite logo" />
-				</a>
-				<a href="https://preactjs.com" target="_blank" rel="noreferrer">
-					<img
-						src={preactLogo}
-						className={`${classes.logo} ${classes.react}`}
-						alt="Preact logo"
-					/>
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className={classes.card}>
-				<button onClick={() => setCount((_count) => _count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/app.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className={classes['read-the-docs']}>
-				Click on the Vite and React logos to learn more
-			</p>
-		</>
-	);
-}
+export const App = () => (
+	<ModalProvider>
+		<div data-testid="wrapper" className={classes.wrapper}>
+			<Home />
+		</div>
+	</ModalProvider>
+);
--- vite.config.ts
+++ vite.config.ts
@@ -2,27 +2,32 @@
 import { defineConfig } from 'vite';
 import path from 'path';
 import react from '@vitejs/plugin-react';
+import { splitVendorChunkPlugin } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	build: {
+		outDir: 'dist',
+	},
+	publicDir: './public',
+	plugins: [react(), splitVendorChunkPlugin()],
 	define: {
 		'import.meta.vitest': 'undefined',
 	},
 	resolve: {
 		alias: {
+			'/src': path.resolve(process.cwd(), 'src'),
+			'@public': path.resolve(__dirname, 'public'),
+			'@assets': path.resolve(__dirname, 'src/assets'),
+			'@components': path.resolve(__dirname, 'src/components'),
+			'@images': path.resolve(__dirname, 'src/assets/images'),
 			'@fonts': path.resolve(__dirname, 'src/assets/fonts'),
+			'@interfaces': path.resolve(__dirname, 'src/interfaces'),
 			'@sass': path.resolve(__dirname, 'src/assets/sass'),
-			'@images': path.resolve(__dirname, './src/assets/images'),
-			'@public': path.resolve(__dirname, './public'),
-			'@components': path.resolve(__dirname, './src/components'),
-		},
-	},
-	css: {
-		preprocessorOptions: {
-			scss: {
-				additionalData: `@use "@sass/main.scss";`,
-			},
+			'@helpers': path.resolve(__dirname, 'src/helpers'),
+			'@pages': path.resolve(__dirname, 'src/pages'),
+			'@configs': path.resolve(__dirname, 'src/configs'),
+			'@services': path.resolve(__dirname, 'src/services'),
 		},
 	},
 	test: {
@@ -36,8 +41,13 @@ export default defineConfig({
 		setupFiles: ['.tests/setup.ts'],
 		includeSource: ['src/**/*.{ts,tsx}'],
 		coverage: {
-			include: ['src/components/**/*.tsx'],
-			exclude: ['src/assets', 'src/**/*.stories.{ts,tsx}'],
+			include: ['src/**/*.tsx'],
+			exclude: [
+				'src/assets',
+				'src/app.tsx',
+				'src/main.tsx',
+				'src/**/*.stories.{ts,tsx}',
+			],
 			provider: 'istanbul',
 			reporter: ['text', 'json', 'html'],
 			reportsDirectory: '.tests/coverage',
